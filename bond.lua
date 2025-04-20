e5d934f68408234ba53ff0a6d5f82458410516fd
ringta9321 <eeeiqjjj159@gmail.com>
2025-04-20 11:21:15 -0400

    Update bond.lua

--- bond.lua
+++ bond.lua
@@ -10,7 +10,7 @@ local hrp = char:WaitForChild("HumanoidRootPart")
 local runtime = workspace:WaitForChild("RuntimeItems")
 
 local foundBonds = {}
-local speed = 3000
+local speed = 4000
 local bond = true -- Activates bond collection
 
 -- Полный массив точек пути (Y = 120)
@@ -105,8 +105,8 @@ spawn(function()
     scanConn:Disconnect()
 
     -- Минимальное время выполнения
-    if tick() - startTime < 60 then
-        task.wait(60 - (tick() - startTime))
+    if tick() - startTime < 35 then
+        task.wait(35 - (tick() - startTime))
     end
 
     -- Внешний скрипт
